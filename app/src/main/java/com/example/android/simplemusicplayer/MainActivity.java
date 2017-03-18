3249a3a368f4fe3285dfcdbedd4c87f8456cf4bd
IT Trouble Shooter <troubles.maker.shooter@gmail.com>
2017-03-18 17:40:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/simplemusicplayer/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.android.simplemusicplayer;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.kali);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void playMusic(View view) {
+
+        mediaPlayer.start();
+    }
+
+    public void stopMusic(View view) {
+
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+
+        }
+
+    }
+
+    private void test(){}
+}
